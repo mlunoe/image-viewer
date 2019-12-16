9e4318fda98c70b5468a11ecf01c3f319d5cd6b6
mlunoe <michael.lunoe@gmail.com>
2019-12-16 23:29:46 +0100

    v.0.0.1

--- src/index.js
+++
@@ -1,5 +0,0 @@
-require('./index.html');
-require('./index.scss');
-
-import './components/App';
-// https://blog.bitsrc.io/can-you-build-web-apps-in-2019-without-a-framework-1065ad82b79f
--- src/utils/getImageID.js
+++
@@ -1,8 +0,0 @@
-export default function getImageID(link) {
-  const segments = link.split('/');
-  const id = segments.pop();
-  if (id) {
-    return id;
-  }
-  return segments.pop();
-};
